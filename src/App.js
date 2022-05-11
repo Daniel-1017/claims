d295354a4da7dbdcb5aaf84962221a77d069d7e2
Daniel-17 <frimudaniel17@gmail.com>
2022-05-11 21:09:12 +0200

    Table scroll

--- src/App.js
+++ src/App.js
@@ -1,10 +1,99 @@
 import React from "react";
-import Menu from "./components/Menu/Menu";
 
 const App = () => {
   return (
     <React.Fragment>
-      <Menu />
+      <h1>Table</h1>
+      <div className="table">
+        <table>
+          <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+          </tr>
+          <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+          </tr>
+        </table>
+      </div>
     </React.Fragment>
   );
 };
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import claimSlice from "./claim-slice";
-
-const store = configureStore({
-  reducer: { claim: claimSlice },
-});
-
-export default store;
